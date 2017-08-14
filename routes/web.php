08de83c96d476ead600e9a08c7f32e28530281d6
workstation\Halim <halim.webdev@gmail.com>
2017-08-14 14:50:47 -0400

    create routes

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    //return view('welcome');
-});
-
 Auth::routes();
-
 
 //Home routes
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+
 
 //Etf routes
 Route::get('/etf', 'EtfController@index');
@@ -28,7 +24,15 @@ Route::get('/etf', 'EtfController@index');
 
 //Users routes
 Route::get('/users', 'UsersController@index');
+Route::get('/users/getdata', 'UsersController@getUsers');
+Route::get('users/create', 'UsersController@create');
+Route::post('users/create', 'UsersController@store');
 
+
+//Route::get('users/{id}', 'UsersController@show');
+//Route::get('users/edit/{id}', 'UsersController@edit');
+//Route::post('users/edit/{id}', 'UsersController@update');
+//Route::delete('users/{id}', 'UsersController@destroy');
 
 
 //About routes
