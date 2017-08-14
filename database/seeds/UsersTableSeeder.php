e5eb89ea4b2ee1fd9b22a53c13433bb6aca10aad
Dell <Dell@Dell-PC>
2017-08-14 17:02:41 -0400

    fix the key too long in serviceprovider

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,9 +13,9 @@ class UsersTableSeeder extends Seeder {
     public function run() {
         
         //DB::table('users')->delete(); //delete all records
-        //App\Models\User::truncate();
+        App\Models\User::truncate();
         
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             
             $username = str_random(6);
         
