bee1cd81797022acbd2337d6f26b30faa038f47d
workstation\Halim <halim.webdev@gmail.com>
2017-08-15 18:16:39 -0400

    add middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -13,33 +13,39 @@
 
 Auth::routes();
 
-//Home routes
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/', 'HomeController@search')->name('home');
+Route::middleware(['auth'])->group(function () {
+   
+    //Home routes
+    Route::get('/', 'HomeController@index')->name('home');
+    //Route::get('/home', 'HomeController@index')->name('home');
+    Route::post('/', 'HomeController@search')->name('home');
+    
+    
+    //Etf routes
+    Route::get('/etf', 'EtfController@index');
+    Route::get('/logs', 'EtfController@logs');
+
+    //Users routes
+    Route::get('/users', 'UsersController@index');
+    Route::get('/users/getdata', 'UsersController@getUsers');
+    Route::get('users/create', 'UsersController@create');
+    Route::post('users/create', 'UsersController@store');
+    Route::get('users/{id}', 'UsersController@show');
+    Route::get('users/{id}/edit/', 'UsersController@edit');
+    Route::post('users/{id}/edit/', 'UsersController@update');
+    Route::delete('users/{id}', 'UsersController@destroy');
+
+    //Change_password routes
+    Route::get('change_password',  'ProfileController@password_form');
+    Route::post('change_password', 'ProfileController@update_password');
+
+    //About & Help routes
+    Route::get('/about', 'HelpController@about');
+    Route::get('/help', 'HelpController@help');
+    
+});
 
 
-//Etf routes
-Route::get('/etf', 'EtfController@index');
-Route::get('/logs', 'EtfController@logs');
-
-//Users routes
-Route::get('/users', 'UsersController@index');
-Route::get('/users/getdata', 'UsersController@getUsers');
-Route::get('users/create', 'UsersController@create');
-Route::post('users/create', 'UsersController@store');
-Route::get('users/{id}', 'UsersController@show');
-
-Route::get('users/{id}/edit/', 'UsersController@edit');
-Route::post('users/{id}/edit/', 'UsersController@update');
-
-Route::delete('users/{id}', 'UsersController@destroy');
-
-//change_password routes
-Route::get('change_password', 'ProfileController@password_form');
-Route::post('change_password', 'ProfileController@update_password');
 
 
-//About routes
-Route::get('/about', 'HelpController@about');
-Route::get('/help', 'HelpController@help');
+
