aa1ed5855ec681d62851bc3156c17872af6ac04b
workstation\Halim <halim.webdev@gmail.com>
2017-08-14 22:17:53 -0400

    work on the search form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Etf;
 
 class HomeController extends Controller {
 
@@ -21,9 +22,49 @@ class HomeController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        //die('xx');
-        return view('home');
+        $title = 'Home';
         
+        
+        
+        
+        return view('home', compact('title'));
+    }
+    
+    /**
+     * Search ETF in database, if not exist check online.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request) {
+        $title = 'Home';
+        
+        $input = trim($request->search);
+        if(!empty($input)){
+            
+            //print_r($request->search); exit;
+            $etf = Etf::where('etf_name', $input)->first();
+            if(!empty($etf)){ // Exist in DB
+                
+                echo '<br>id='.$etf['etf_id'];
+                echo '<br>des='.$etf['description'];
+                
+                
+                //print_r($etf); 
+                exit;
+            }
+            else{ //Not exist in DB => Search in website
+                
+                
+                
+                
+            }
+            
+            
+            die('makach');
+            
+            
+        }
+        return view('home', compact('title'));
     }
 
 }
