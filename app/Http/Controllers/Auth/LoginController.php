715e824049141d3522924bd34a989d6eaa37b3ee
workstation\Halim <halim.webdev@gmail.com>
2017-08-15 22:14:09 -0400

    overide login method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,6 +28,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+    protected $redirectAfterLogout = '/login';
 
     /**
      * Create a new controller instance.
@@ -44,5 +47,20 @@ class LoginController extends Controller
     public function showLoginForm() {
         $title = 'Login';
         return view('auth.login', compact('title'));
+    }
+    
+    
+     /*
+     * Authentication
+     */
+    public function login(Request $request) {
+        $email    = $request['email'];
+        $password = $request['password'];
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            return redirect()->intended($this->redirectTo);
+        } 
+        else {
+            return back()->withErrors(array('Email or password incorrect!'))->withInput();
+        }
     }
 }
