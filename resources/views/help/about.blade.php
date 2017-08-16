4d6b4447384ee5cc9f44a16d32de22be830570a9
workstation\Halim <halim.webdev@gmail.com>
2017-08-16 00:47:14 -0400

    work on about blade

--- resources/views/help/about.blade.php
+++ resources/views/help/about.blade.php
@@ -7,12 +7,24 @@
             <h3 class="page-title" id="business-info"><u>About:</u></h3> 
         </div>
 
-        
         <div class="col-md-12">
+            <p style="line-height: 32px;">
+                This application is created for a Test Assignment purpose.<br>
+                <strong>Version:</strong> 1.0.0<br>
+                <strong>Author:</strong> Halim Lardjane<br>
+                <strong>Email:</strong> halim.lardjane@gmail.com<br>
+                <strong>Website:</strong> <a href="http://halim.lardjane.com/" target="_blank">http://halim.lardjane.com/</a><br> 
+            </p>
             
-            
-            
-            
+            <p style="margin-top: 40px">
+                <strong><u>Technologies used:</u></strong>
+            </p>
+            <ul style="line-height:32px">
+                <li><a href="https://laravel.com/" target="_blank">PHP Laravel Framework (5.4)</a></li>
+                <li><a href="http://getbootstrap.com/" target="_blank">CSS Bootstrap</a></li>
+                <li><a href="https://jquery.com/" target="_blank">jQuery</a></li>
+                <li><a href="https://www.highcharts.com/" target="_blank">Highcharts</a></li>
+            </ul>
             
         </div>
         
