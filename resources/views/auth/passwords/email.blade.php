f6ab6fb43a0a85a41cc20d4f7321a13564c259bf
workstation\Halim <halim.webdev@gmail.com>
2017-08-15 22:19:32 -0400

    fix some small html

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.auth')
 
 @section('content')
-
 <div id="main">
     <div id="wrapper" class="Center-Container">
         <div id="page-content-wrapper">
@@ -10,12 +9,14 @@
                     <div class="col">
                         <div class="col-md-6 col-sm-offset-3 box">
 
-                            <?php if(isset($this->notif) and !empty($this->notif)){ ?>
-                            <div class="alert alert-<?php  ?> alert-dismissable">
+                            @if($errors->any()) 
+                            <ul class="errors alert alert-danger alert-dismissable">
                                 <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                                <?php echo $this->notif['msg']; ?>
-                            </div>
-                            <?php } ?>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            @endif
 
                             <div id="loginbox" style="" class="mainbox">                    
                                 <div class="panel panel-primary" >
@@ -23,16 +24,20 @@
                                         <div class="panel-title pull-left">Test Assignment - Reset Password</div>
                                     </div>     
                                     <div style="padding-top:40px" class="panel-body" >
-                                        <form method="POST" action="login" class="form-horizontal" role="form">
-                                            <div style="margin-bottom: 25px" class="input-group">
+                                        <form method="POST" action="{{ route('password.email') }}" class="form-horizontal" role="form">    
+                                            {{ csrf_field() }}
+                                            
+                                            <div class="input-group" style="margin-bottom: 25px">
                                                 <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                                <input type="email" class="form-control" id="email" name="email" value="<?php echo @$_POST['email'];?>" placeholder="Email address" required>                                        
+                                                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Email address" required>                                        
                                             </div>
-                                            <div style="margin-top:10px" class="form-group">
+                                            
+                                            <div class="form-group" style="margin-top:10px" >
                                                 <div class="col-sm-12 controls">
                                                     <button type="submit" class="btn btn-primary">Reset</button>
                                                 </div>
                                             </div>
+                                            
                                             <div class="form-group">
                                                 <div class="col-md-12 control">
                                                     <div style="border-top: 1px solid#888; padding-top:15px;font-size:95%" >
@@ -40,6 +45,7 @@
                                                     </div>
                                                 </div>
                                             </div> 
+                                            
                                         </form>     
                                     </div>                     
                                 </div>  
@@ -52,6 +58,4 @@
         </div><!-- #page-content-wrapper -->
     </div><!-- #wrapper -->
 </div><!-- #main -->
-
-
 @endsection
