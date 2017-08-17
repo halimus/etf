7d21516a663da36f2b83f31ec851cf7664eec081
Halim Lardjane <halim.lardjane@gmail.com>
2017-08-17 13:11:27 -0400

    Update about.blade.php

--- resources/views/help/about.blade.php
+++ resources/views/help/about.blade.php
@@ -21,9 +21,11 @@
             </p>
             <ul style="line-height:32px">
                 <li><a href="https://laravel.com/" target="_blank">PHP Laravel Framework (5.4)</a></li>
+                <li><a href="https://www.mysql.com/" target="_blank">MySQL Database</a></li>
                 <li><a href="http://getbootstrap.com/" target="_blank">CSS Bootstrap</a></li>
                 <li><a href="https://jquery.com/" target="_blank">jQuery</a></li>
                 <li><a href="https://www.highcharts.com/" target="_blank">Highcharts</a></li>
+                <li><a href="https://www.amcharts.com/" target="_blank">amCharts</a></li>
             </ul>
             
         </div>
